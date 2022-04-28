2455322ef876d5d4d197685a77d639e5399bff5b
Ali Sbane <ali.sbane.dev@gmail.com>
2022-04-28 19:40:25 +0000

    Form Error added, and label placeholder functionality fixed

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+
 import spotifyLogo from "../assets/logos/spotify.png";
 import applePodLogo from "../assets/logos/apple-pod.png";
 import googlePodLogo from "../assets/logos/google-pod.png";
 import pocketCastsLogo from "../assets/logos/pocket-casts.png";
 
 export default function Header() {
+  let [emailValue, setEmailValue] = useState("");
+  let [emailValid, setEmailValid] = useState(true);
+
+  function handleSubmit(e) {
+    if (validateEmail(emailValue)) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+      e.preventDefault();
+    }
+  }
+
+  function handleChange(e) {
+    setEmailValue(e.target.value);
+  }
+
   return (
     <div className="content">
       <div className="content__text">
@@ -23,15 +41,30 @@ export default function Header() {
         <img src={googlePodLogo} alt="" />
         <img src={pocketCastsLogo} alt="" />
       </div>
-      <form action="" className="content__form">
-
+      <form onSubmit={handleSubmit} action="" className="content__form">
         <div className="content__input-box">
-          <input id="email" type="text" />
+          <input
+            id="email"
+            type="text"
+            value={emailValue}
+            onChange={handleChange}
+          />
           <label htmlFor="email">Email address</label>
         </div>
-
+        <p className={`content__form-error ${emailValid ? "d-n" : ""}`}>
+          Oops! Please check your email
+        </p>
         <button className="content__form-btn">Request Access</button>
       </form>
     </div>
   );
+}
+
+// Helper function to check if an email is valid, just copied from internet
+
+function validateEmail(email) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return true;
+  }
+  return false;
 }
