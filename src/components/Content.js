52ef2e0e05cbb5ba43f76c447d5b4d4b44ca5010
Ali Sbane <ali.sbane.dev@gmail.com>
2022-04-29 18:09:26 +0000

    Tablet version completed

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,6 +5,14 @@ import applePodLogo from "../assets/logos/apple-pod.png";
 import googlePodLogo from "../assets/logos/google-pod.png";
 import pocketCastsLogo from "../assets/logos/pocket-casts.png";
 
+// Desktop versions of logos
+import spotifyLogoDesktop from "../assets/logos/spotify-desktop.png";
+import applePodLogoDesktop from "../assets/logos/apple-pod-desktop.png";
+import googlePodLogoDesktop from "../assets/logos/google-pod-desktop.png";
+import pocketCastsLogoDesktop from "../assets/logos/pocket-casts-desktop.png";
+
+import patternPoints from "../assets/images/pattern-points.png";
+
 export default function Header() {
   let [emailValue, setEmailValue] = useState("");
   let [emailValid, setEmailValid] = useState(true);
@@ -23,39 +31,53 @@ export default function Header() {
   }
 
   return (
-    <div className="content">
-      <div className="content__text">
-        <h1>
-          Publish your podcasts
-          <span className="white">everywhere.</span>
-        </h1>
-        <p className="content__text__p">
-          Upload your audio to Pod with a single click. We’ll then distribute
-          your podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket Casts
-          and more!
-        </p>
-      </div>
-      <div className="content__sponsors">
-        <img src={spotifyLogo} alt="" />
-        <img src={applePodLogo} alt="" />
-        <img src={googlePodLogo} alt="" />
-        <img src={pocketCastsLogo} alt="" />
-      </div>
-      <form onSubmit={handleSubmit} action="" className="content__form">
-        <div className="content__input-box">
-          <input
-            id="email"
-            type="text"
-            value={emailValue}
-            onChange={handleChange}
-          />
-          <label htmlFor="email">Email address</label>
+    <div className="wrapper">
+      <div className="content">
+        <div className="content__text">
+          <h1>
+            Publish your podcasts
+            <span className="white">everywhere.</span>
+          </h1>
+          <p className="content__text__p">
+            Upload your audio to Pod with a single click. We’ll then distribute
+            your podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket
+            Casts and more!
+          </p>
         </div>
-        <p className={`content__form-error ${emailValid ? "d-n" : ""}`}>
-          Oops! Please check your email
-        </p>
-        <button className="content__form-btn">Request Access</button>
-      </form>
+        <div className="content__sponsors">
+          <div className="d-m">
+            <img src={spotifyLogo} alt="" />
+            <img src={applePodLogo} alt="" />
+            <img src={googlePodLogo} alt="" />
+            <img src={pocketCastsLogo} alt="" />
+          </div>
+
+          {/* Desktop version of logos */}
+
+          <div className="d-d">
+            <img src={spotifyLogoDesktop} alt="" />
+            <img src={applePodLogoDesktop} alt="" />
+            <img src={googlePodLogoDesktop} alt="" />
+            <img src={pocketCastsLogoDesktop} alt="" />
+          </div>
+        </div>
+        <form onSubmit={handleSubmit} action="" className="content__form">
+          <div className="content__input-box">
+            <input
+              id="email"
+              type="text"
+              value={emailValue}
+              onChange={handleChange}
+            />
+            <label htmlFor="email">Email address</label>
+          </div>
+          <p className={`content__form-error ${emailValid ? "d-n" : ""}`}>
+            Oops! Please check your email
+          </p>
+          <button className="content__form-btn">Request Access</button>
+        </form>
+      </div>
+      <img className="design__pattern-points" src={patternPoints} alt="" />
     </div>
   );
 }
